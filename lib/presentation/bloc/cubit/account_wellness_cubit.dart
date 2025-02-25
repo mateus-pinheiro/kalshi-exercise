ec2bb4f93004a72d045e49d91a4f35d65af7950e
Mateus <mateus.pinheiro@terceiro-sky.com.br>
2025-02-25 12:12:17 -0300

    fix: adjusting unit tests

--- lib/presentation/bloc/cubit/account_wellness_cubit.dart
+++ lib/presentation/bloc/cubit/account_wellness_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kalshi_exercise/domain/entities/account_wellness_entity.dart';
 import 'package:kalshi_exercise/domain/usecases/get_account_wellness_usecase.dart';
-import 'package:kalshi_exercise/presenter/bloc/state/account_wellness_state.dart';
+import 'package:kalshi_exercise/presentation/bloc/state/account_wellness_state.dart';
 
 class AccountWellnessCubit extends Cubit {
   AccountWellnessCubit(
--- test/account_wellness_usecase_test.dart
+++ test/account_wellness_usecase_test.dart
@@ -6,26 +6,45 @@ import 'package:kalshi_exercise/domain/entities/account_wellness_enum.dart';
 void main() {
   late GetAccountWellnessUsecase usecase;
   late AccountWellnessEntity accountWellnessEntity;
+  late AccountWellnessStatus statusResult;
 
   setUp(() {
     usecase = GetAccountWellnessUsecase();
   });
 
-  test('account wellness healthy', () {
+  test('account wellness healthy successfully', () {
     accountWellnessEntity = AccountWellnessEntity(1000, 10);
-    final result = usecase.call(accountWellnessEntity);
-    expect(result, AccountWellnessStatus.healthy);
+    final either = usecase.call(accountWellnessEntity);
+
+    either.fold(
+      (failure) => null,
+      (status) => statusResult = status,
+    );
+
+    expect(statusResult, AccountWellnessStatus.healthy);
   });
 
-  test('account wellness medium', () {
+  test('account wellness medium successfully', () {
     accountWellnessEntity = AccountWellnessEntity(1000, 30);
-    final result = usecase.call(accountWellnessEntity);
-    expect(result, AccountWellnessStatus.medium);
+    final either = usecase.call(accountWellnessEntity);
+
+    either.fold(
+      (failure) => null,
+      (status) => statusResult = status,
+    );
+
+    expect(statusResult, AccountWellnessStatus.medium);
   });
 
-  test('account wellness low', () {
+  test('account wellness low successfully', () {
     accountWellnessEntity = AccountWellnessEntity(1000, 80);
     final result = usecase.call(accountWellnessEntity);
-    expect(result, AccountWellnessStatus.low);
+
+    result.fold(
+      (failure) => null,
+      (status) => statusResult = status,
+    );
+
+    expect(statusResult, AccountWellnessStatus.low);
   });
 }
